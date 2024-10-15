3a72a3361c66c9f193aea7ed39b4121f8feb4946
sonnyitsunny <sun9999042@gmail.com>
2024-10-15 17:14:24 +0900

    Init Project

---
+++ 3DPoseEstimation/3DPoseEstimation/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  3DPoseEstimation
+//
+//  Created by 손동현 on 10/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Start: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
